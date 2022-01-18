035004cd1e135623fff00b3f64ecdaec6a8ffd8d
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-01-18 20:09:56 +0700

    create login and register page interface

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showRegisterForm() {
+        return view('auth.register');
+    }
+
+    public function showLoginForm() {
+        return view('auth.login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function ()
+{
+    return view('index');
+});
+
+Route::prefix('/auth')->group(function() {
+    Route::get('/register', [AuthController::class, 'showRegisterForm']);
+    Route::get('/login', [AuthController::class, 'showLoginForm']);
 });
